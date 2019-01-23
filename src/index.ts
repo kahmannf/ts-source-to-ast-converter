a0c544cdb5a685cdcd14d459984a461a4019f777
kahmannf <felix@kahmann.de>
2019-01-23 18:20:28 +0100

    fix strang ídex.ts bug (í vs i), change read file back to argv

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { existsSync, readFileSync } from 'fs';
+import * as ts from 'typescript';
+
+// tslint:disable-next-line:no-magic-numbers
+const fileName: string = process.argv[2];
+
+if (!fileName || !existsSync(fileName)) {
+  process.exit(1);
+}
+
+const sourceFile = ts.createSourceFile(fileName, readFileSync(fileName).toString(), ts.ScriptTarget.ES2015, true);
+
+// tslint:disable-next-line:no-console
+console.log(JSON.stringify(sourceFile, (key, value) => {
+  if ( key === 'parent') {
+    return'__[CIRCULAR]__';
+  } else {
+    return value;
+  }
+}));
+
+process.exit(0);
