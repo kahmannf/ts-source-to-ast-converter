8137e5d5828f7944b507948b9496ccbfd4b84089
kahmannf <felix@kahmann.de>
2019-01-23 18:18:11 +0100

    finish script, move it to index.ts

--- src/îndex.ts
+++ src/îndex.ts
@@ -2,7 +2,7 @@ import { existsSync, readFileSync } from 'fs';
 import * as ts from 'typescript';
 
 // tslint:disable-next-line:no-magic-numbers
-const fileName: string = process.argv[2];
+const fileName: string = 'F:\\Projekte\\ts-source-to-ast-converter\\src\\îndex.ts';
 
 if (!fileName || !existsSync(fileName)) {
   process.exit(1);
@@ -11,6 +11,12 @@ if (!fileName || !existsSync(fileName)) {
 const sourceFile = ts.createSourceFile(fileName, readFileSync(fileName).toString(), ts.ScriptTarget.ES2015, true);
 
 // tslint:disable-next-line:no-console
-console.log(JSON.stringify(sourceFile));
+console.log(JSON.stringify(sourceFile, (key, value) => {
+  if ( key === 'parent') {
+    return'__[CIRCULAR]__';
+  } else {
+    return value;
+  }
+}));
 
 process.exit(0);
